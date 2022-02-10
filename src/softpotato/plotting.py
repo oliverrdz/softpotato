e52ffaabd66b268e39d8b26fde37794c7693fd8a
Oliver Rdz <oliver.rdz@gmail.com>
2022-02-10 11:54:08 -0600

    Added ability to plot multiple curves on the same figure

--- src/softpotato/plotting.py
+++ src/softpotato/plotting.py
@@ -11,6 +11,37 @@ def format(xlab, ylab, show):
         plt.show()
 
 def plot(x, y, xlab='$E$ / V', ylab='$i$ / A', mark='-', fig=1, show=False):
+    '''
+        Function to plot curves without all the hassle. By default, y is a list 
+        and mark is a string. To use different markers for each curve, include them as 
+        a list where each item is a different marker / line style. y can also be a
+        numpy array.
+
+        Example:
+        > import numpy as np
+        > x = np.array([1,2,3,4])
+        > y = [x, x**2, x***3]
+        > mark = ['-', '--', 'or-']
+        > sp.plotting.plot(x, y, xlab='x', ylab='y', mark=mark, show=1)
+    '''
+    # By default, y should be a list, if it is just a numpy array then
+    # convert it into list to be used in the for loop afterwards:
+    if (type(y) != list):
+        y = [y]
+    ny = len(y)
+    # Even with multiple plots, the user may want to have the same marker or
+    # line stile for each curve, so if it is not a list, convert it into one
+    if (type(mark) != list):
+        mark = [mark]*ny
     plt.figure(fig)
-    plt.plot(x, y, mark)
+    for n in range(ny):
+        plt.plot(x, y[n], mark[n])
     format(xlab, ylab, show)
+
+
+if __name__ == '__main__':
+    import numpy as np
+    x = np.array([1,2,3,4])
+    y = [x, x**2, x**3]
+    mark = ['-vg', '--b', '-or']
+    plot(x, y, mark=mark, show=1)
