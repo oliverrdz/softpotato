321e2e880770352d08267176fcf780089f6905f7
Oliver Rdz <oliver.rdz@gmail.com>
2022-02-10 12:23:15 -0600

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='softpotato',
-    version='0.0.24',
+    version='0.0.25',
     license='GPL3',
     author='Oliver Rodriguez',
     author_email='oliver.rdz@gmail.com',
