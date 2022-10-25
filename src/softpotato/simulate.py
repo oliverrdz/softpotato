010fc188938081b1816addf9817f3dee2ed1fcd8
Oliver Rodriguez <oliver.rdz@gmail.com>
2022-10-25 16:03:18 -0500

    Back to FD

--- src/softpotato/simulate.py
+++ src/softpotato/simulate.py
@@ -1,237 +1,107 @@
 import numpy as np
-from scipy.sparse import diags
 
-'''
-    Pending:
-    - Add C mechanism
-    - Calculate concentration of P on the EC and C mechanisms
-
-    Errors:
-    - Denormalization of concentrations is wrong
-'''
-
+## Electrochemistry constants
 F = 96485 # C/mol, Faraday constant    
 R = 8.315 # J/mol K, Gas constant
 T = 298 # K, Temperature
 FRT = F/(R*T)
 
+
 class E:
-    '''
-        Defines an E species
-    '''
-    def __init__(self, n=1, DO=1e-5, DR=1e-5, cOb=0, cRb=1e-6, E0=0, ks=1e8, 
-                 alpha=0.5):
+    def __init__(self, wf, n=1, A=0.0314, E0=0, cOb=0, cRb=1e-6, 
+                 DO=1e-5, DR=1e-5, k0=1e8, alpha=0.5):
+        self.wf = wf
         self.n = n
-        self.DO = DO
-        self.DR = DR
-        self.DOR = DO/DR
+        self.A = A
+        self.E0 = E0
         self.cOb = cOb
         self.cRb = cRb
-        self.E0 = E0
-        self.ks = ks
+        self.DO = DO
+        self.DR = DR
+        self.k0 = k0
         self.alpha = alpha
+        self.DOR = DO/DR
+
+    def grid(self):
+        self.nT = np.size(self.wf.t)
+        self.dT = 1/self.nT
+        self.lamb = 0.45
+        self.Xmax = 6*np.sqrt(self.nT*self.lamb)
+        self.dX = np.sqrt(self.dT/self.lamb)
+        self.nX = int(self.Xmax/self.dX)
+
+        ## Discretisation of variables and initialisation
+        if self.cRb == 0: # In case only O present in solution
+            self.CR = np.zeros([self.nT,self.nX])
+            self.CO = np.ones([self.nT,self.nX])
+        else:
+            self.CR = np.ones([self.nT,self.nX])
+            self.CO = np.ones([self.nT,self.nX])*self.cOb/self.cRb
+
+        self.X = np.linspace(0,self.Xmax,self.nX) # Discretisation of distance
+        self.eps = (self.wf.E-self.E0)*self.n*FRT # adimensional potential waveform
+        self.delta = np.sqrt(self.DR*self.wf.t[-1]) # cm, diffusion layer thickness
+        self.K0 = self.k0*self.delta/self.DR # Normalised standard rate constant
 
 
-class C:
-    '''
-        Defines a C species  
-    '''
-    def __init__(self, DP=1e-5, cPb=0, kc=1e-2):
-        self.DP = DP
-        self.cPb = cPb
-        self.kc = kc
-        #self.Kc = 0
-
-
-class TGrid:
-    '''
-        Defines the grid in time
-    '''
-    def __init__(self, twf, Ewf):
-        self.t = twf
-        self.E = Ewf
-        self.nT = np.size(self.t) # number of time elements
-        self.dT = 1/self.nT # adimensional step time
-        self.T = twf/twf[-1] # adimensional time
-
-        
-class XGrid:
-    '''
-        Defines the grid in space
-    '''
-    def __init__(self, species, tgrid, Ageo=1):
-        self.lamb = 0.45 # For the algorithm to be stable, lamb = dT/dX^2 < 0.5
-        self.Xmax = 6*np.sqrt(tgrid.nT*self.lamb) # Infinite distance
-        self.dX = np.sqrt(tgrid.dT/self.lamb) # distance increment
-        self.nX = int(self.Xmax/self.dX) # number of distance elements
-        self.X = np.linspace(0, self.Xmax, self.nX) # Discretisation of distance
-        self.Ageo = Ageo
-
-        for x in species:
-            if isinstance(x, E):
-                ## Discretisation of variables and initialisation
-                if x.cRb == 0: # In case only O present in solution
-                    x.CR = np.zeros([tgrid.nT, self.nX])
-                    x.CO = np.ones([tgrid.nT, self.nX])
-                else:
-                    x.CR = np.ones([tgrid.nT, self.nX])
-                    x.CO = np.ones([tgrid.nT, self.nX])*x.cOb/x.cRb
-
-                x.eps = (tgrid.E-x.E0)*x.n*FRT # adimensional potential waveform
-                x.delta = np.sqrt(x.DR*tgrid.t[-1]) # cm, diffusion layer thickness
-                x.Ke = x.ks*x.delta/x.DR # Normalised standard rate constant
-                # Construct matrix:
-                Cb = np.ones(self.nX-1) # Cbefore
-                Cp = -2*np.ones(self.nX) # Cpresent
-                Ca = np.ones(self.nX-1) # Cafter
-                x.A = diags([Cb,Cp,Ca], [-1,0,1]).toarray()/(self.dX**2) 
-                x.A[0,:] = np.zeros(self.nX)
-                x.A[0,0] = 1 # Initial condition
-            elif isinstance(x, C):
-                x.CP = np.zeros([tgrid.nT, self.nX])
-
-
-class Simulate:
-    '''
-    '''
-    def __init__(self, species, mech, tgrid, xgrid):
-        self.species = species
-        self.mech = mech
-        self.tgrid = tgrid
-        self.xgrid = xgrid
-
-        self.join(species)
-
-    def sim(self):
-        nE = self.nE[0]
-        nC = self.nC[0]
-        sE = self.species[nE]
-        sC = self.species[nC]
-        for k in range(1, self.tgrid.nT):
+    def run(self):
+        self.grid()
+        for k in range(1,self.nT):
             # Boundary condition, Butler-Volmer:
-            CR1kb = sE.CR[k-1,1]
-            CO1kb = sE.CO[k-1,1]
-            sE.CR[k,0] = (CR1kb + self.xgrid.dX*sE.Ke*np.exp(-sE.alpha*sE.eps[k]
-                         )*(CO1kb + CR1kb/sE.DOR))/(1 + self.xgrid.dX*sE.Ke*(
-                         np.exp((1-sE.alpha)*sE.eps[k])+np.exp(
-                         -sE.alpha*sE.eps[k])/sE.DOR))
-            sE.CO[k,0] = CO1kb + (CR1kb - sE.CR[k,0])/sE.DOR
-            # Runge-Kutta 4:
-            sE.CR[k,1:-1] = self.RK4(sE.CR[k-1,:], 'E', sE)[1:-1]
-            if self.mech == 'E':
-                sE.CO[k,1:-1] = self.RK4(sE.CO[k-1,:], 'E', sE)[1:-1]
-            elif self.mech == 'EC':
-                sE.CO[k,1:-1] = self.RK4(sE.CO[k-1,:], 'EC', sE, sC)[1:-1]
-                sC.CP[k,0] = sC.CP[k-1,1]
-                sC.CP[k,1:-1] = self.RK4(sC.CP[k-1,:], 'ECP', sE, sC)[1:-1]
-                
-        for s in self.species:
-            if isinstance(s, E):
-                # Denormalising:
-                if s.cRb:
-                    I = -s.CR[:,2] + 4*s.CR[:,1] - 3*s.CR[:,0]
-                    D = s.DR
-                    c = s.cRb
-                else: # In case only O present in solution
-                    I = s.CO[:,2] - 4*s.CO[:,1] + 3*s.CO[:,0]
-                    D = s.DO
-                    c = s.cOb
-                self.i = s.n*F*self.xgrid.Ageo*D*c*I/(2*self.xgrid.dX*s.delta)
+            CR1kb = self.CR[k-1,1]
+            CO1kb = self.CO[k-1,1]
+            self.CR[k,0] = (CR1kb + self.dX*self.K0*np.exp(
+                            -self.alpha*self.eps[k])*(CO1kb + CR1kb/self.DOR))/(
+                       1 + self.dX*self.K0*(np.exp((1-self.alpha)*self.eps[k])+\
+                       np.exp(-self.alpha*self.eps[k])/self.DOR))
+            self.CO[k,0] = CO1kb + (CR1kb - self.CR[k,0])/self.DOR
 
-                #self.cR = s.CR*s.cRb
-                #self.cO = s.CO*s.cOb
-                self.x = self.xgrid.X*s.delta
-            elif isinstance(s, C):
-                self.cP = s.CP#*s.cPb
+            self.CR[k,1:-1] = self.CR[k-1,1:-1] + self.lamb*(self.CR[k-1,2:]\
+                            - 2*self.CR[k-1,1:-1] + self.CR[k-1,:-2])
+            self.CO[k,1:-1] = self.CO[k-1,1:-1] + self.lamb*(self.CO[k-1, 2:]\
+                            - 2*self.CO[k-1, 1:-1] + self.CO[k-1,:-2])
 
-        s.CR = 0 # Empty matrix to free memory
-        s.CO = 0
-        s.CP = 0
+        # Denormalising:
+        if self.cRb:
+            I = -self.CR[:,2] + 4*self.CR[:,1] - 3*self.CR[:,0]
+            D = self.DR
+            c = self.cRb
+        else: # In case only O present in solution
+            I = self.CO[:,2] - 4*self.CO[:,1] + 3*self.CO[:,0]
+            D = self.DO
+            c = self.cOb
+        self.i = self.n*F*self.A*D*c*I/(2*self.dX*self.delta)
 
-    def join(self, species):
-        ns = len(species)
-        self.nE = []
-        self.nC = []
-        for s in range(ns):
-            if isinstance(species[s], E):
-                self.nE.append(s)
-            elif isinstance(species[s], C):
-                self.nC.append(s)
-        # User may only want to simulate an E mechanism:
-        if not self.nC:
-            self.nC.append(0)
-        if not self.nE:
-            self.nE.append(0)
+        self.cR = self.CR*self.cRb
+        self.cO = self.CO*self.cOb
+        self.x = self.X*self.delta
 
-        if self.mech == 'EC' or self.mech == 'C':
-            species[self.nC[0]].Kc = species[self.nC[0]].kc* \
-                                     species[self.nE[0]].delta/ \
-                                     species[self.nE[0]].DR
-
-    def fun(self, y, mech, species, params):
-        rate = 0
-        if mech == 'EC':
-            rate = - self.tgrid.dT*params.Kc*y
-        elif mech == 'ECP': # To obtain concentration profile of P
-            rate = self.tgrid.dT*params.Kc*y
-        return np.dot(species.A, y) + rate
-
-    def RK4(self, y, mech, species, params=0):
-        dT = self.tgrid.dT
-        k1 = self.fun(y, mech, species, params)
-        k2 = self.fun(y+dT*k1/2, mech, species, params)
-        k3 = self.fun(y+dT*k2/2, mech, species, params)
-        k4 = self.fun(y+dT*k3, mech, species, params)
-        return y + (dT/6)*(k1 + 2*k2 + 2*k3 + k4)
-
+            
 
 
 if __name__ == '__main__':
-    from calculate import *
-    from technique import *
-    from plotting import *
+    import softpotato as sp
+    print('Running from main')
 
-    # CV
-    wf = Sweep(Eini=0.5, Efin=-0.5, sr=0.01)
-    twf = wf.t
-    Ewf = wf.E
-    e = E(cRb=0, cOb=1e-6)
-    tgrid = TGrid(twf, Ewf)
-    xgrid = XGrid([e], tgrid)
-    simE = Simulate([e], 'E', tgrid, xgrid)
-    simE.sim()
-    e = E(cRb=0, cOb=1e-6)
-    c = C(kc=1e-1)
-    xgrid = XGrid([e,c], tgrid)
-    simEC = Simulate([e,c], 'EC', tgrid, xgrid)
-    simEC.sim()
-    plot(Ewf, [simE.i, simEC.i], xlab='$E$ / V', ylab='$i$ / A', 
-         legend=['E', 'EC'], fig=1, show=0)
+    # Potential waveform:
+    wf = sp.technique.Sweep(Eini=-0.5, Efin=0.5, sr=0.1)
 
-    # CA
-    wf = Step(Es=-0.4, dt=0.01)
-    twf = wf.t
-    Ewf = wf.E
-    tgrid = TGrid(twf, Ewf)
-    e = E(cRb=0, cOb=1e-6)
-    xgrid = XGrid([e], tgrid)
-    simE = Simulate([e], 'E', tgrid, xgrid)
-    simE.sim()
-    e = E(cRb=0, cOb=1e-6)
-    c = C(kc=1e-1)
-    xgrid = XGrid([e,c], tgrid)
-    simEC = Simulate([e,c], 'EC', tgrid, xgrid)
-    simEC.sim()
+    # Electrochemical parameters:
+    n = 1
+    A = 0.0314
+    E0 = 0
+    cOb = 0
+    cRb = 1e-6
+    DO = 3.25e-5
+    DR = 3.25e-5
+    k0 = 1e8
+    alpha = 0.5
 
-    # Cottrell
-    macro = Macro()
-    i_cott = macro.Cottrell(twf)
+    # Simulation:
+    sim = E(wf, n, A, E0, cOb, cRb, DO, DR, k0, alpha)
+    sim.run()
+    
+    # Plotting
+    sp.plotting.plot(wf.t, wf.E, xlab='$t$ / s', ylab='$E$ / V', fig=1, show=0)
+    sp.plotting.plot(wf.E, sim.i*1e6, ylab='$i$ / $\mu$A', fig=2, show=1)
 
-    legend = ['E', 'EC', 'Cottrell']
-    mark = ['-o', 'o', '--']
-    plot(1/np.sqrt(twf[1:]), [simE.i[1:], simEC.i[1:], i_cott[1:]], 
-         xlab='$t^{-1/2}$ / s$^{-1/2}$', ylab='$i$ / A', 
-         legend=legend, mark=mark, fig=2, show=0)
-    plot(twf[1:], [simE.i[1:]/i_cott[1:], simEC.i[1:]/i_cott[1:]],
-         xlab='$t$ / s', ylab='$i$ / $i_{cott}$',
-         legend=legend, mark=mark, fig=3, show=1)
