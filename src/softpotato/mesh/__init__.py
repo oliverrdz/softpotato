5da67a923eb3f21b95230bcf9a49495638b7e86d
Oliver Rodriguez <oliver.dz@gmail.com>
2026-07-27 20:59:32 +0100

    Deleted all files in the repository.

--- src/softpotato/mesh/__init__.py
+++
@@ -1,4 +0,0 @@
-from softpotato.core.abcs import BaseMesh
-from softpotato.mesh.uniform_1d import Uniform1DMesh
-
-__all__ = ["BaseMesh", "Uniform1DMesh"]
--- src/softpotato/solvers/__init__.py
+++
@@ -1,4 +0,0 @@
-from softpotato.core.abcs import BaseSolver
-from softpotato.solvers.ode_solver import ODESolver, SimulationResult
-
-__all__ = ["BaseSolver", "ODESolver", "SimulationResult"]
